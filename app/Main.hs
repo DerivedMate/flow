19ef4938349b13a9c0b13aaf3fa799108de49bca
derivedmate <tosurowiec@gmail.com>
2021-08-10 16:37:29 +0200

    add static optimization via FOPT; add AClosure, AKeep anchors; add exhaustStep; port functions to closures; add exhaustive conditional step; add ackermann test; change static cell reduction to MNone only;

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,10 @@
 module Main where
+import           Data.Bifunctor
 import           Data.List
 import           Eval
 import           Eval.RT
+import           Reduction.Reducer              ( optimize )
+import           Reduction.Static               ( rStaticExp )
 import           Syntax
 import           System.Directory.Internal.Prelude
                                                 ( fromMaybe )
@@ -33,13 +36,22 @@ interactiveShell =
     | otherwise
     = runFlow (parseString input) >> loop
 
+runFile :: Int -> FilePath -> IO ()
+runFile optLvl path = optSrc >>= runFlow
+ where
+  srcParse = parseFile path
+  optSrc | optLvl == 1 = fmap (first (optimize rStaticExp)) <$> srcParse
+         | otherwise   = srcParse
+
+
 main :: IO ()
 main = do
-  isDev <- (== Just "dev") <$> lookupEnv "FENV"
+  isDev  <- (== Just "dev") <$> lookupEnv "FENV"
+  optLvl <- maybe 0 read <$> lookupEnv "FOPT"
   if isDev
-    then parseFile "test/pg.hf" >>= runFlow
+    then runFile optLvl "test/pg.hf"
     else do
       args <- getArgs
       case args of
         []             -> interactiveShell
-        (filePath : _) -> parseFile filePath >>= runFlow
+        (filePath : _) -> runFile optLvl filePath
